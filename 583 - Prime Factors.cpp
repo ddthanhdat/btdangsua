96cbc5f289dd0b0619fe604c55159edd02a6aa1d
do thanh dat <ddthanhdat@gmail.com>
2015-05-23 22:33:59 +0700

    Update 583 - Prime Factors.cpp

--- 583 - Prime Factors.cpp
+++ 583 - Prime Factors.cpp
@@ -2,19 +2,50 @@
 #include <string.h>
 #include <math.h>
 using namespace std;
-void ngto(long long n){
+const int maxn=60000;
+bool isPrime[maxn];
+int mang[maxn]; int fn;
+void Prime(){
+	for(long long i=2;i<=maxn;i++){
+		isPrime[i]=true;
+	}
+	for(long long i=2;i<=maxn;i++){
+		if(isPrime[i])
+			for(long long j=i;i*j<=maxn;j++)
+				isPrime[i*j]=false;
+	}
+	fn=0;
+	for(long long i=2;i<=maxn;i++){
+		if(isPrime[i]) mang[fn++]=i;
+	}
+}
+bool ngto(long long n){
+	int i=0;
+	while(mang[i]<=sqrt(n)){
+		if(n%mang[i++]==0) return 0;
+	}
+	return 1;
+}
+void factor(long long n){
+	
 	bool dem=0;
+	
 	for(long long i=2;i<=n;i++){
+		if(ngto(n)==1) {
+			cout << n;
+			return;
+		}
 		if(n%i==0){
 			if(dem==1) cout << " x ";
 			cout << i ;
 			n=n/i;
-			i--;
 			dem=1;
+			i--;
 		}
 	}
 }
 int main(){
+	Prime();
 	long long n;
 	while(cin >> n){
 		if(!n) break;
@@ -23,7 +54,7 @@ int main(){
 			cout << "-1 x ";
 			n*=-1;
 		}
-		ngto(n);
+		factor(n);
 		cout << endl;
 	}
 	
